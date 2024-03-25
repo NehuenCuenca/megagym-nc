473d4b7d3a5e7d9ee2adb8bb6bc3ad2811909843
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-03-25 00:07:37 -0300

    Replace two image of members

--- src/assets/members/members.js
+++ src/assets/members/members.js
@@ -1,7 +1,7 @@
 
 import picture1 from './hombre-press-maquina.webp';
 import picture2 from './mujer-polea-baja.webp';
-import picture3 from './hombre-sentadilla.webp';
-import picture4 from './mujer-remo-mancuerna.webp';
+import picture3 from './remo-hombre.webp';
+import picture4 from './press-hombros-mujer.webp';
 
 export const members = [ picture3.src, picture2.src, picture1.src, picture4.src];
