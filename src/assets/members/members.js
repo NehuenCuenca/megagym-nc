2f03bd86d38780e82f9cb4d8520daa882797cfe6
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-03-27 12:49:11 -0300

    Add new images and removed those that are not used on the CTA carousel

--- src/assets/members/members.js
+++
@@ -1,7 +0,0 @@
-
-import picture1 from './hombre-press-maquina.webp';
-import picture2 from './mujer-polea-baja.webp';
-import picture3 from './remo-hombre.webp';
-import picture4 from './press-hombros-mujer.webp';
-
-export const members = [ picture3.src, picture2.src, picture1.src, picture4.src];
